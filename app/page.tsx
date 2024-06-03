ad20e20c74e44ac7c5d68f2f39fc3961a0b6838e
JiHye Yoon <peppermint.xoxo@gmail.com>
2024-06-02 17:15:08 -0700

    cleared npm run lint

--- app/page.tsx
+++ app/page.tsx
@@ -22,7 +22,7 @@ export default function Home() {
 
       <div className="relative z-[1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
         <h1 className="relative z[2] text-center">
-          <div className="text-6xl font-bold text-black">Where's my</div>
+          <div className="text-6xl font-bold text-black">Where&apos;s my</div>
           <div className="text-8xl font-bold text-black">WEGOVY</div>
         </h1>
       </div>
